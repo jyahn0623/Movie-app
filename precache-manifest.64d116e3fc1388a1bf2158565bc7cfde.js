9c51f32ce933a494b74cbbe4a341a9fb1d7cac88
pualahn <jyahn0623@gmail.com>
2019-12-21 14:09:40 +0900

    Updates

---
+++ precache-manifest.64d116e3fc1388a1bf2158565bc7cfde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21411d1b0fbc1c5ded26ed2e88a4aa51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ddb6105421dcfeeded",
+    "url": "/static/css/main.28185750.chunk.css"
+  },
+  {
+    "revision": "5ee0d3bc805c315fa2e8",
+    "url": "/static/js/2.80ddaf7c.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.80ddaf7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1ddb6105421dcfeeded",
+    "url": "/static/js/main.7e55314b.chunk.js"
+  },
+  {
+    "revision": "b76936937929263b0065",
+    "url": "/static/js/runtime-main.0d625899.js"
+  }
+]);
